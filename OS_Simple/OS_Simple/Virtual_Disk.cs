d8519650f01245ae643d1d5582e2d496390c913e
ahmed khfaga <idkmaster89@gmail.com>
2024-11-14 19:09:12 +0200

    adding some commentint

--- OS_Simple/OS_Simple/Virtual_Disk.cs
+++ OS_Simple/OS_Simple/Virtual_Disk.cs
@@ -42,19 +42,19 @@ namespace OS_Simple
 
         }
 
-        public static void writeCluster(byte[] cluster , int clusterIndex)
+        public static void writeCluster(byte[] clusterdata , int clusterIndex)
         {
             //Virtual_Disk.CreateOrOpen_Disk();
             //byte[] buffer = Encoding.ASCII.GetBytes(cluster.ToArray());
             //disk.Seek(clusterIndex * clusterSize, SeekOrigin.Begin);
             //disk.Write(buffer, 0, buffer.Length);
             //disk.Flush();
-            if (cluster.Length > clusterSize )
+            if (clusterdata.Length > clusterSize )
             {
                 throw new ArgumentException($"Cluster must be {clusterSize} bytes");
             }
             disk.Seek(clusterIndex * clusterSize, SeekOrigin.Begin);
-            disk.Write(cluster, 0, clusterSize);
+            disk.Write(clusterdata, 0, clusterSize);
             disk.Flush(); 
         }
         public static byte[] readCluster(int clusterIndex) 
@@ -68,16 +68,20 @@ namespace OS_Simple
         // METHODS GETFREE_SPACE
         public static int getFreeSpace()
         {
-            disk.Seek(0, SeekOrigin.End);
-            return diskSize - (int)disk.Position;
+            disk.Seek(0, SeekOrigin.End); //// Go to the end of the file and get the position
+            //long diisk = disk.Length; we can calculate length of disk like this also 
+            return diskSize - (int)disk.Position; // diskSize(1024*1024) - int(disk.Length)
         }
 
+        //Checks if the disk is new by seeing if the file length is 0
         public static bool isNew()
         {
+
+            // like FreeSpace if Disk.Length is == 0 so disk is New 
             disk.Seek(0,SeekOrigin.End);
 
             int size = (int)disk.Position;
-
+            // int size = Disk.Length we can do also like this ... 
             return size == 0;
         }
 
