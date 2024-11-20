b6efba2c6547d301afa89c99230bd282c723b46a
ahmed khfaga <idkmaster89@gmail.com>
2024-11-20 05:45:27 +0200

    some update in class Directory_Entry to adding method assignDirName

--- OS_Simple/OS_Simple/Directory_Entry.cs
+++ OS_Simple/OS_Simple/Directory_Entry.cs
@@ -16,24 +16,57 @@ namespace OS_Simple
 
         public Directory_Entry(char[] Dir_Name, byte dir_Attribute,int f_Cluster,int dir_FileSize)
         {
-            string s = new string(Dir_Name);
-            if (Dir_Name.Length < 11)
-            {
-                for (int i = Dir_Name.Length; i < 11; i++)
-                {
-                    s += (char)' ';
-                }
-            }
-            this.Dir_Namee = s.ToCharArray();
-           // this.Dir_Namee = Dir_Name;
+            #region Test0
+            //string s = new string(Dir_Name);
+            //if (Dir_Name.Length < 11)
+            //{
+            //    for (int i = Dir_Name.Length; i < 11; i++)
+            //    {
+            //        s += (char)' ';
+            //    }
+            //}
+            //this.Dir_Namee = s.ToCharArray();
+            // this.Dir_Namee = Dir_Name; 
+            #endregion
+
+            assignDirName(Dir_Name);
             this.dir_Attr = dir_Attribute;
             this.dir_First_Cluster = f_Cluster;
             this.dir_FileSize = dir_FileSize;
-            for(int i=0;i<Dir_Empty.Length;i++) 
-            {
-                Dir_Empty[i] = 0;
-            }
+            //for(int i=0;i<Dir_Empty.Length;i++) 
+            //{
+            //    Dir_Empty[i] = 0;
+            //}
+
+            // Initilize Dir_Empty to zero 
+            Array.Clear(Dir_Empty, 0, Dir_Empty.Length);
              
         }
+        public void assignDirName(char[] name)
+        {
+            // Initilize Dir_Namee  
+
+            Array.Clear(Dir_Namee,0,Dir_Namee.Length);
+
+            #region test1
+            //string s = new string(name);
+
+            //if(name.Length < 11) 
+            //{
+            //    for(int i = name.Length; i < 11; i++)
+            //    {
+            //        s += (char)' ';
+            //    }
+            //} 
+            #endregion
+
+            for(int i=0;i < name.Length && i < Dir_Namee.Length;i++)
+            {
+                Dir_Namee[i] = name[i];
+            }
+            // if name less than 11 
+            for (int i = name.Length; i < Dir_Namee.Length; i++)
+                Dir_Namee[i] = ' ';
+        }
     }
-}
+}   
